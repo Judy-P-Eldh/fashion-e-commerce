7730ffda592b7b493220c1abf5d1f6f4603e6394
Andreas Granér <Andreas@graner.me>
2025-09-09 16:22:50 +0200

    feat: create skeleton for start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-
+import Footer from '@/components/footer';
 
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <section></section>
+      <section></section>
+      <Footer />
+    </main>
+  );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+export default function Footer() {
+  return (
+    <footer>
+      <aside></aside>
+
+      <nav>
+        <img src="" alt="logo" />
+        <section>
+          <h3>About</h3>
+          <ul>
+            <Link href=""></Link>
+          </ul>
+        </section>
+        <section></section>
+        <section></section>
+      </nav>
+    </footer>
+  );
+}
