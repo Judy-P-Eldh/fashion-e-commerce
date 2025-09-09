b91aa0d5352e6f71010e0cbda623d4938239cc11
Pernilla Eldh <pernilla.eldh@gmail.com>
2025-09-09 16:09:09 +0200

    Lagt till fetch för produkterna från API. Lagt till möjlighet att visa bilder från API.

---
+++ lib/interfaces/product.ts
@@ -0,0 +1,27 @@
+export interface Product {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    tags: string[];
+    stock: number;
+    brand: string;
+    weight: number;
+    dimensions: { width: number; height: number, depth: number };
+    shippingInformation: string;
+    availabilityStatus: string;
+    reviews: { reviwerName: string; comment: string; rating: number }[];
+    returnPolicy: string;
+    images: string[];
+    thumbnail: string;
+}
+
+export interface ProductResponse {
+    limit: number;
+    products: Product[];
+    skip: number;
+    total: number;
+}
