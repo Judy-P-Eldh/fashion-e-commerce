031bfb511a402e9b2b7c0f5ccd2649717c7874f4
Andreas Granér <Andreas@graner.me>
2025-09-09 15:04:09 +0200

    feat: search component, basic structure of search component

--- components/search.tsx
+++ components/search.tsx
@@ -1,3 +1,10 @@
-export default function search() {
-  return <input type="text" />;
+import Form from 'next/form';
+
+export default function search({ query }: { query?: string }) {
+  return (
+    <Form action="/search">
+      <input type="text" defaultValue={query} name="search-input" />
+      <button type="submit">search</button>
+    </Form>
+  );
 }
