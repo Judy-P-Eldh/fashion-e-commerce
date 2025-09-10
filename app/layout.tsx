521822e1560e9ca3a88d5294616a2d7e70f3f29d
Andreas Granér <Andreas@graner.me>
2025-09-10 11:11:37 +0200

    FIx: typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navigation/>
-        {children}</body>
-      <body>
         {children}
         <Footer />
       </body>
