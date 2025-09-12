66e07927496d2d77dd31b62c659a6dc493d24292
Andreas Granér <Andreas@graner.me>
2025-09-12 12:29:11 +0300

    WIP: create start page with most sold products

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
+import { fetchProducts } from '@/lib/data/productdata';
+import { Product } from '@/lib/interfaces/product';
+import Form from 'next/form';
+import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const products = await fetchProducts(10);
+
+  const firstForProducts = Array.isArray(products) ? products.slice(0, 4) : null;
+
   return (
-    <main className="min-h-dvh">
-      <section></section>
-      <section></section>
+    <main className="full-width ">
+      <section className="content-grid">
+        <h2>Sign up for our newsletter</h2>
+        <Form action="submit">
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input type="text" />
+        </Form>
+      </section>
+      <section className="full-width">
+        <h2>Best sellers</h2>
+        <ul>
+          {firstForProducts?.map((p: Product) => {
+            return (
+              <li key={p.id} className="text-white">
+                {p.title}
+                <Image src={p.images[0]} alt={p.description} height={600} width={300}></Image>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </main>
   );
 }
--- lib/data/productdata.ts
+++ lib/data/productdata.ts
@@ -1,17 +1,30 @@
-import { Category, Product } from "../interfaces/product";
+import { Category, Product } from '../interfaces/product';
 
-const endpoint = "https://dummyjson.com/products/category/";
+const endpoint = 'https://dummyjson.com/products';
 
-export async function fetchProducts(category: Category): Promise<Product[] | { message: string }> {
-    try {
-        const response = await fetch(`${endpoint}${category}`, { cache: 'no-store' });
-        if (!response.ok) return { message: 'Failed to fetch sunglasses.' }
-        const data = await response.json();
-        console.log(data);
-        return data.results;  
-    } catch (error) {
-        console.log(error);
-        throw new Error("Could not reach the API to fetch sunglasses.")
-    }
+export async function fetchProducts(limit: number = 0): Promise<Product[] | { message: string }> {
+  try {
+    const response = await fetch(`${endpoint}?limit=${limit}`, { cache: 'no-store' });
+    if (!response.ok) return { message: 'Failed to fetch products.' };
+    const data = await response.json();
+    return data.products;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Could not reach the API to fetch products.');
+  }
 }
 
+export async function fetchProductsFromCategory(
+  category: Category
+): Promise<Product[] | { message: string }> {
+  try {
+    const response = await fetch(`${endpoint}/category/${category}`, { cache: 'no-store' });
+    if (!response.ok) return { message: 'Failed to fetch sunglasses.' };
+    const data = await response.json();
+    console.log(data);
+    return data.results;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Could not reach the API to fetch sunglasses.');
+  }
+}
