2bb04fa5d7ef2d741ae05b1532067c36db4b35fb
Andreas Granér <andreas@graner.me>
2025-09-16 13:29:46 +0200

    WIP: params to change collection

--- app/collections/[collection]/page.tsx
+++ app/collections/[collection]/page.tsx
@@ -1,20 +1,30 @@
 import { collections } from '@/lib/data/variables';
 import { CollectionKey } from '@/lib/interfaces/product';
 import Link from "next/link"
-export default async function Page({ params }: { params: { selectedCollection?: CollectionKey } }) {
+export default async function Page({ params }: { params: Promise<{ selectedCollection?: CollectionKey }> }) {
   let { selectedCollection = "women" } = await params;
-  
+  console.log(selectedCollection);
+
 
   return (
-    <div>
-      <section></section>
-      <ul>{
+    <div className='content-grid'>
+      <section className='content flex justify-evenly'>
+        {
+          Object.keys(collections).map((collection) => {
+            return (
+              <Link key={collection} href={`${collection}`} className={selectedCollection === collection ? 'bg-green' : 'bg-black'}>{collection}</Link>
+
+            )
+          })
+        }
+      </section>
+      <ul className='breakout grid grid-cols-3'>{
         collections[selectedCollection].map((category) => {
-            return <li key={category}>
-                <Link href={`search?category=${category}`}>{category}</Link>
-            </li>
+          return <li key={category}>
+            <Link href={`/search?category=${category}`}>{category}</Link>
+          </li>
         })
-        }</ul>
+      }</ul>
     </div>
   );
 }
