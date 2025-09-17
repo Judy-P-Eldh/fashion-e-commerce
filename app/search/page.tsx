03d7f857684eb5c1309e33f379690d16c5dc38ec
Pernilla Eldh <pernilla.eldh@gmail.com>
2025-09-17 14:29:19 +0200

    Added pagination on search page.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,25 +1,48 @@
 import ProductCard from "@/components/productcard";
-import { fetchFilteredProducts } from "@/lib/data/productdata";
+import { fetchBySearch } from "@/lib/data/productdata";
 import { Product } from "@/lib/interfaces/product";
 import Link from "next/link";
 
 export default async function SearchPage({
   searchParams,
 }: {
-  searchParams: Promise<{ query: string }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
-  const { query } = await searchParams;
+  const { query, page = 1 } = await searchParams; // hämtar sökquery och sätter page till 1 som start
+  const pageNum = Number(page); // konverterar page (som är string) till number för framtida hantering
+  const limit = 6; // ska visa 6 produkter per sida
+  const skip = (pageNum - 1) * limit; // räknar ut hur många produkter i API:et som ska hoppas över på sidan (sida1: (1-1)*6=0 -> startar från 0, sida2: (2-1)*6=6 -> hoppar över första 6)
 
-  const products = await fetchFilteredProducts(query);
+  const result = await fetchBySearch(query, limit, skip);
+
+  if (typeof result === "object" && "message" in result)
+    return <p> {result.message}</p>;
+
+  const { products, total } = result;
+  const maxPage = Math.ceil(total / limit); // räknar ut
 
   return (
     <main>
       <section>
         <h2>Products</h2>
-        <ul className="grid md:grid-cols-3" id="card-ul">
+        <div className="flex gap-4 justify-center">
+          <Link
+            className="p-2 border-2 rounded-sm m-2"
+            href={`/search?query=${query}&page=${pageNum - 1}`}
+          >
+            Previous
+          </Link>
+          <Link
+            className="p-2 border-2 rounded-sm m-2"
+            href={`/search?query=${query}&page=${pageNum + 1}`}
+          >
+            Next
+          </Link>
+        </div>
+        <ul className="grid md:grid-cols-3 gap-2 mx-4" id="card-ul">
           {products.length > 0 ? (
             products.map((product: Product) => (
-              <li key={product.id}>
+              <li className="" key={product.id}>
                 <Link id="product-card" href={`/products/${product.id}`}>
                   <ProductCard product={product} />
                 </Link>
--- lib/data/productdata.ts
+++ lib/data/productdata.ts
@@ -1,5 +1,4 @@
 import {
-  allowedCategories,
   Category,
   Product,
   ProductResponse,
@@ -48,33 +47,33 @@ export async function fetchProductsByCategory(
 }
 
 export async function fetchBySearch(
-  query: string
-): Promise<Product[] | { message: string }> {
+  query: string = "", limit: number = 0, skip: number
+): Promise<ProductResponse | { message: string }> {
   try {
-    const response = await fetch(`${endpoint}/search?q=${query}`);
+    const response = await fetch(`${endpoint}/search?q=${query}&limit=${limit}&skip=${skip}`);
     if (!response.ok) return { message: `Failed to fetch ${query}.` };
     const data = await response.json();
-    return data.products ?? [];
+    return data;
   } catch (error) {
     console.log(error);
     throw new Error(`Could not reach the API to fetch ${query}.`);
   }
 }
 
-export async function fetchFilteredProducts(query: string) {
-  const objectsFromSearch = await fetchBySearch(query);
+// export async function fetchFilteredProducts(query: string) {
+//   const objectsFromSearch = await fetchBySearch(query);
 
-  const allowedCategoriesSet = new Set(allowedCategories);
-  if (
-    objectsFromSearch &&
-    typeof objectsFromSearch === "object" &&
-    "message" in objectsFromSearch
-  ) {
-    return []; //Hantera om fel uppstår.
-  }
+//   const allowedCategoriesSet = new Set(allowedCategories);
+//   if (
+//     objectsFromSearch &&
+//     typeof objectsFromSearch === "object" &&
+//     "message" in objectsFromSearch
+//   ) {
+//     return []; //Hantera om fel uppstår.
+//   }
 
-  const chosenProducts = objectsFromSearch.filter((product) =>
-    allowedCategoriesSet.has(product.category as Category)
-  );
-  return chosenProducts;
-}
+//   const chosenProducts = objectsFromSearch.filter((product) =>
+//     allowedCategoriesSet.has(product.category as Category)
+//   );
+//   return chosenProducts;
+// }
