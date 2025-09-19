fbbae371de1922321632e2f61d15321074f06813
malpisen <malin.rydefalk@gmail.com>
2025-09-19 11:51:37 +0200

    made productcard stylable, moved link and button inside card

--- components/productcard.tsx
+++ components/productcard.tsx
@@ -1,19 +1,26 @@
 import { Product } from "@/lib/interfaces/product";
+import Link from "next/link";
 import Image from "next/image";
+import BuyButton from "./buybutton";
 
 export default function ProductCard({ product }: { product: Product }) {
   const image = product.images[0];
   return (
-    <>
-      <h2>{product.title}</h2>
-      <Image
-        className="order-first"
-        src={image}
-        alt={product.title}
-        width={400}
-        height={400}
-      />
-      <p>$ {product.price}</p>
-    </>
+    <article className="border">
+      <Link href={`/products/${product.id}`}>
+        <div className="grid">
+          <h2>{product.title}</h2>
+          <Image
+            className="order-first"
+            src={image}
+            alt={product.title}
+            width={400}
+            height={400}
+          />
+          <p>$ {product.price}</p>
+        </div>
+      </Link>
+      <BuyButton productName={product.title} />
+    </article>
   );
 }
