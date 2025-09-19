d09d09a1620c27428a38f5668f5c144b50385dbd
Andreas Granér <Andreas@graner.me>
2025-09-19 11:12:57 +0200

    feat: Move navigation links to client component and add underline to active pathway

---
+++ components/navigation-links.tsx
@@ -0,0 +1,37 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function NavigationLinks() {
+  const pathname = usePathname();
+  const links = [
+    { name: 'Home', href: '/' },
+    { name: 'Collections', href: '/collections/women' },
+    { name: 'SALE', href: '/sale' },
+  ];
+  const isActive = (href: string) => {
+    console.log(pathname, href, mounted);
+
+    return pathname === href || pathname.startsWith(href + '/');
+  };
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <nav className="grid">
+      <ul className="flex items-center text-2xl">
+        {links.map((link) => {
+          return (
+            <li
+              key={link.name}
+              className={`${mounted && isActive(link.href) ? 'underline' : ''} mr-10`}
+            >
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,39 +1,25 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import Search from './search';
 import { User, ShoppingBag } from 'lucide-react';
+import NavigationLinks from './navigation-links';
 
 export default function Navigation() {
   return (
-    <header className="h-60 text-black content-grid ">
+    <header className="h-45 text-black content-grid ">
       <div className="breakout grid-cols-3 grid">
-        <nav className="grid">
-          <ul className="flex items-center">
-            <li className='mr-8'>
-              <Link href="/">Home</Link>
-            </li>
-            <li className='mr-8'>
-              <Link href="/collections/women">Collections</Link>
-            </li>
-            <li>
-              <Link href="#">SALE</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex justify-center">
-          <picture className="flex items-center">
-            <User></User>
-          </picture>
+        <NavigationLinks />
+        <div className="flex justify-center items-center">
+          <h1 className="text-9xl">LOGO</h1>
         </div>
         <aside className="flex gap-5 items-center justify-end">
           <Search btn={false} />
           <Link href="#">
             {' '}
-               <User></User>
+            <User strokeWidth={1.5} size={35}></User>
           </Link>
           <Link href="#">
             {' '}
-           <ShoppingBag></ShoppingBag>
+            <ShoppingBag strokeWidth={1.5} size={35}></ShoppingBag>
           </Link>
         </aside>
       </div>
