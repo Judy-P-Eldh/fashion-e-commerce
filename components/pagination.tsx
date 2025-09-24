c56d8ad72b03acfaf5185706e5b68a0da286d3f0
malpisen <malin.rydefalk@gmail.com>
2025-09-24 17:41:42 +0200

    Cherry-pick: check if baseUrl has query or not (resolved conflict in pagination.tsx)

---
+++ components/pagination.tsx
@@ -0,0 +1,33 @@
+import { PaginationProps } from "@/lib/interfaces/pagination";
+import Link from "next/link";
+
+export default function Pagination({
+  currentPage,
+  maxPage,
+  baseUrl,
+}: PaginationProps) {
+  const separator = baseUrl.includes("?") ? "&" : "?";
+  return (
+    <div className="flex gap-4 justify-center">
+      {currentPage > 1 && (
+        <Link
+          className="p-2 border-2 rounded-sm"
+          href={`${baseUrl}${separator}page=${currentPage - 1}`}
+        >
+          Previous
+        </Link>
+      )}
+
+      <span className="p-2">{`Page ${currentPage} of ${maxPage}`}</span>
+
+      {currentPage < maxPage && (
+        <Link
+          className="p-2 border-2 rounded-sm"
+          href={`${baseUrl}${separator}page=${currentPage + 1}`}
+        >
+          Next
+        </Link>
+      )}
+    </div>
+  );
+}
