85a1792a78fbab12ec5396a7f0c9b4f1962353e5
malpisen <malin.rydefalk@gmail.com>
2025-09-24 17:42:49 +0200

    implemented functionality on category page

--- app/collections/[selectedCollection]/[selectedCategory]/page.tsx
+++ app/collections/[selectedCollection]/[selectedCategory]/page.tsx
@@ -1,11 +1,51 @@
 import { AllowedCategory } from "@/lib/interfaces/product";
+import { fetchProductsByCategory } from "@/lib/data/productdata";
+import ProductList from "@/components/productlist";
+import Pagination from "@/components/pagination";
+import { categoryLabels } from "@/lib/data/variables";
 
 export default async function CategoryPage({
   params,
+  searchParams,
 }: {
-  params: Promise<{ selectedCategory: AllowedCategory }>;
+  params: Promise<{
+    selectedCollection: string;
+    selectedCategory: AllowedCategory;
+  }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
-  const { selectedCategory } = await params;
-    //la in detta temporärt för att se att jag fick in kategorien och underlätta för den som ska göra denna :)
-  return <div>{selectedCategory}</div>;
+  const { selectedCollection, selectedCategory } = await params;
+  const { page = "1" } = await searchParams;
+  const pageNum = Number(page);
+  const limit = 4;
+  const skip = (pageNum - 1) * limit;
+
+  const result = await fetchProductsByCategory(selectedCategory, limit, skip);
+
+  if (typeof result === "object" && "message" in result)
+    return <p> {result.message}</p>;
+
+  const { products, total } = result;
+  const maxPage = Math.ceil(total / limit);
+
+  return (
+    <section className="content-grid">
+      <div className="content">
+        <h2 className="text-center">{categoryLabels[selectedCategory]}</h2>
+        {products.length > 0 ? (
+          <ProductList products={products} />
+        ) : (
+          <p>No products found.</p>
+        )}
+      </div>
+
+      <div className="content flex gap-4 justify-center">
+        <Pagination
+          currentPage={pageNum}
+          maxPage={maxPage}
+          baseUrl={`/collections/${selectedCollection}/${selectedCategory}`}
+        />
+      </div>
+    </section>
+  );
 }
