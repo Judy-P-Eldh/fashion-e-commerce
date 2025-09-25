dc184e216ba52b24875f5c970296448ac24dba8b
malpisen <malin.rydefalk@gmail.com>
2025-09-25 15:49:24 +0200

    initial styling

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({
 
   if (!product) notFound();
   return (
-/*     <main className="min-h-dvh grid">
+    /*     <main className="min-h-dvh grid">
       <article>
         <Image
           src={product.thumbnail}
@@ -27,33 +27,31 @@ export default async function Page({
         <p>${product.price}</p>
       </article>
       <BuyButton productName={product.title} />
-    </main> */
+    </main> 
+    */
 
+    <main className="max-w-4xl items-center justify-center m-5 grid grid-cols-1 md:grid-cols-2 gap-8 ">
+      <div className="flex items-center justify-center">
+        <Image
+          src={product.thumbnail}
+          alt={product.title}
+          width={400}
+          height={400}
+          className="rounded-lg object-cover shadow-lg bg-light"
+        />
+      </div>
 
-    <main className="items-center justify-center grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl w-full p-6">
-        {/* Produktbild */}
-        <div className="flex items-center justify-center bg-light">
-          <Image
-            src={product.thumbnail}
-            alt={product.title}
-            width={400}
-            height={400}
-            className="rounded-lg object-cover shadow-md"
-          />
+      <div className="flex flex-col">
+        <div>
+          <h2 className="font-bold mb-4">{product.title}</h2>
+          <p className="mb-6">{product.description}</p>
         </div>
 
-        {/* Produktinfo */}
-        <div className="flex flex-col justify-between">
-          <header>
-            <h2 className="text-3xl font-bold mb-4">{product.title}</h2>
-            <p className="text-gray-700 mb-6">{product.description}</p>
-          </header>
-
-          <div className="flex flex-col gap-4">
-            <p className="text-2xl font-semibold">${product.price}</p>
-            <BuyButton productName={product.title} />
-          </div>
+        <div className="flex flex-col items-start gap-4">
+          <p className="text-2xl font-semibold">${product.price}</p>
+          <BuyButton productName={product.title} />
         </div>
+      </div>
     </main>
   );
 }
