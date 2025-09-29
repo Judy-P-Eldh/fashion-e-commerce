e5a381528d22cca27ae2586f44ccff41c78b588e
malpisen <malin.rydefalk@gmail.com>
2025-09-29 11:07:44 +0200

    added picture to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,26 @@
+import Image from "next/image";
+
 export default function AboutPage() {
-    return (
-        <main className="content-grid">
-            <article className="content">
-                <h2>About CompanyName</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam officia, non rerum pariatur ipsam sed ex quam! Porro impedit fugiat dicta quasi, ex reprehenderit deleniti repellat harum veniam error aliquid?</p>
-            </article>
-        </main>
-    )
+  return (
+    <main className="content-grid">
+      <article className="content-small m-5 gap-5 grid grid-cols-1 md:grid-cols-2">
+        <Image
+          src={"/about.jpg"}
+          alt={""}
+          width={500}
+          height={400}
+          className="rounded-lg object-cover shadow-lg bg-light place-self-center"
+        />
+        <div className="flex flex-col gap-5">
+          <h2>About CompanyName</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam
+            officia, non rerum pariatur ipsam sed ex quam! Porro impedit fugiat
+            dicta quasi, ex reprehenderit deleniti repellat harum veniam error
+            aliquid?
+          </p>
+        </div>
+      </article>
+    </main>
+  );
 }
