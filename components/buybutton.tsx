5fb1f5547f13ee583e7a6c857c124357664d10f1
Andreas Granér <Andreas@graner.me>
2025-10-01 11:19:22 +0200

    Feature: Add toaster when user adds to cart and signs up for newsletter

--- components/buybutton.tsx
+++ components/buybutton.tsx
@@ -1,10 +1,11 @@
-"use client";
-import { inShoppingCart } from "@/lib/data/variables";
-import { ShoppingBagIcon } from "lucide-react";
+'use client';
+import { inShoppingCart } from '@/lib/data/variables';
+import { ShoppingBagIcon } from 'lucide-react';
+import toast from 'react-hot-toast';
 export default function BuyButton({ productName }: { productName: string }) {
   function handleClick() {
     inShoppingCart.push(productName);
-    alert(`${productName} added to shopping cart.`);
+    toast.success(`${productName} added to shopping cart.`);
   }
   return (
     <button
---
+++ components/sign-up-form.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Form from 'next/form';
+import toast from 'react-hot-toast';
+
+export default function SignUpForm() {
+  const addToMailingList = (formData: FormData) => {
+    const email = formData.get('email') as string;
+
+    toast.success(`Thanks for signing up! Your discount code will be sent to ${email}`, {duration: 4000});
+  };
+
+  return (
+    <Form className="breakout" action={addToMailingList}>
+      <h2 className="mb-5 font-courier-prime text-xl">
+        Sign up for our newsletter and get 10% off your first order
+      </h2>
+      <label htmlFor="email" className="sr-only">
+        Email
+      </label>
+      <input type="text" required name="email" className="bg-light" />
+      <button type="submit">Sign me up</button>
+    </Form>
+  );
+}
